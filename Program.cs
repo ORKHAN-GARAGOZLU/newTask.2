b15fc93db2866173168d7a069852a2cebfe65c75
User <User@DESKTOP-EVDBN8V>
2023-02-06 15:40:35 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace newTask._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Massivin uzunlugu:");
+            int n = int.Parse(Console.ReadLine());
+            double[] mass = new double[n];
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                mass[i] = int.Parse(Console.ReadLine());
+            }
+
+            for ( int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(mass[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
